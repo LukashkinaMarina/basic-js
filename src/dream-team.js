07d923550185638f6070bfd68c637c01f17f5435
Marina Lukashkina <lukaskinamarina1@gmail.com>
2021-09-29 11:39:20 +0300

    init: add solutions for few tasks

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,19 +1,29 @@
-import { NotImplementedError } from '../extensions/index.js';
+import { NotImplementedError } from "../extensions/index.js";
 
 /**
  * Create name of dream team based on the names of its members
- *  
- * @param {Array} members names of the members 
+ *
+ * @param {Array} members names of the members
  * @return {String | Boolean} name of the team or false
  * in case of incorrect members
  *
  * @example
- * 
+ *
  * createDreamTeam(['Matt', 'Ann', 'Dmitry', 'Max']) => 'ADMM'
  * createDreamTeam(['Olivia', 1111, 'Lily', 'Oscar', true, null]) => 'LOO'
  *
  */
-export default function createDreamTeam(/* members */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function createDreamTeam(dream) {
+  let x = [];
+  if (!dream) return false;
+  for (let i = 0; i < dream.length; i++) {
+    if (typeof dream[i] === "string") {
+      x.push(dream[i]);
+    }
+  }
+  let name = x
+    .map((e) => e.split(" ").join("").slice(0, 1).toUpperCase())
+    .sort()
+    .join("");
+  return name;
 }
