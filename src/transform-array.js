dfa3b0cf68cf8b599a75f861caa047818b330cc5
Marina Lukashkina <lukaskinamarina1@gmail.com>
2021-10-04 15:32:52 +0300

    feat: add solution

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,19 +1,37 @@
-import { NotImplementedError } from '../extensions/index.js';
+import { NotImplementedError } from "../extensions/index.js";
 
 /**
  * Create transformed array based on the control sequences that original
  * array contains
- * 
+ *
  * @param {Array} arr initial array
  * @returns {Array} transformed array
- * 
+ *
  * @example
- * 
+ *
  * transform([1, 2, 3, '--double-next', 4, 5]) => [1, 2, 3, 4, 4, 5]
  * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
- * 
+ *
  */
-export default function transform(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function transform(array) {
+  if (!Array.isArray(array))
+    throw new Error("'arr' parameter must be an instance of the Array!");
+  const a = [...array];
+  for (let i = 0; i < a.length; i++) {
+    if (a[i] === "--discard-next") {
+      a[i] = undefined;
+      a[i + 1] = undefined;
+    }
+    if (a[i] === "--double-next") {
+      a[i] = a[i + 1];
+    }
+    if (a[i] === "--double-prev") {
+      a[i] = a[i - 1];
+    }
+    if (a[i] === "--discard-prev") {
+      a[i] = undefined;
+      a[i - 1] = undefined;
+    }
+  }
+  return a.filter((a) => a !== undefined);
 }
